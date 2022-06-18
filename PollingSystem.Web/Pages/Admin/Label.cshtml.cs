38520d71a5b02627fe5c51a89700af50aab6c1b2
Иван <84615148+INDRA909@users.noreply.github.com>
2022-06-18 21:45:52 +0500

    Razor login page(wiht cookie autentification)

---
+++ PollingSystem.Web/Pages/Admin/Label.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PollingSystem.Web.Pages.Admin
+{
+    public class LabelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
